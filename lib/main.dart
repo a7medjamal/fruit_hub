32c09c46d951f8e4e0dbc10e6760e1b7ccffed70
a7medjamal <ahmedjmal20003@gmail.com>
2025-08-16 14:45:59 +0300

    feat: add localization support with intl package and configure app for multiple languages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruit_hub/core/utils/app_router.dart';
 
 void main() {
@@ -11,6 +12,14 @@ class FruitHubApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [Locale('en'), Locale('ar')],
+
+      locale: const Locale('ar'),
       routerConfig: AppRouter.router,
       theme: ThemeData(scaffoldBackgroundColor: Colors.white),
     );
