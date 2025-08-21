57c3e0fa7dcd031756c98f612e142ef2cacacc2d
a7medjamal <ahmedjmal20003@gmail.com>
2025-08-21 16:40:03 +0300

    feat: integrate Firebase with dotenv support and update project configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruit_hub/core/utils/app_router.dart';
+import 'package:fruit_hub/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env"); // Load the env variables
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const FruitHubApp());
 }
 
